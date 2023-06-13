c144f0bf1d5c3deb6c5ceee6cd0aadab60905828
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-13 22:34:21 +0300

    KANAMBO-BOT

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -8,11 +8,11 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`${ssd}🟢 *pong* : ${latensi.toFixed(4)} _ms_`);
+          m.reply(`${ssd}👩‍💻⚡ *speed test...* : ${latensi.toFixed(4)} _ms_`);
             });
 }
 handler.help = ['ping']
 handler.tags = ['main']
-handler.command = ['ping', 'speed']
+handler.command = ['ping', 'speed', 'test']
 
 export default handler
