e0278f2943c8dbc373aeacd51b532048bbf3aec2
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-16 15:40:50 +0300

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,13 +1,14 @@
+
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 import yts from 'yt-search'
 var handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `Use example ${usedPrefix}${command} Rihanna man down`
+  if (!text) throw `Use example ${usedPrefix}${command} naruto blue bird`
   await m.reply(wait)
   let search = await yts(text)
   let vid = search.videos[Math.floor(Math.random() * search.videos.length)]
   if (!search) throw 'Video Not Found, Try Another Title'
   let { title, thumbnail, timestamp, views, ago, url } = vid
-  let wm = 'Downloading audio please wait for kanambo-bot'
+  let wm = 'Downloading audio please wait'
 
   let captvid = `╭──── 〔 Y O U T U B E 〕 ─⬣
 ⬡ Title: ${title}
@@ -22,31 +23,31 @@ var handler = async (m, { conn, command, text, usedPrefix }) => {
 ⬡ Views: ${views}
 ⬡ Upload: ${ago}
 ⬡ Link: ${url}
-╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['VIDEO', `${usedPrefix}ytmp4 ${url}`], false, { quoted: m, 'document': { 'url':'https://wa.me/254114148625' },
+╰────────⬣`, author.trim(), await( await conn.getFile(thumbnail)).data, ['VIDEO', `${usedPrefix}ytmp4 ${url}`], false, { quoted: m, 'document': { 'url':'https://wa.me/917605902011' },
 'mimetype': global.dpdf,
-'fileName': `KANAMBO ℙ𝕃𝔸𝕐𝔼ℝ`,
+'fileName': `𝔾𝕌ℝ𝕌 ℙ𝕃𝔸𝕐𝔼ℝ`,
 'fileLength': 666666666666666,
 'pageCount': 666,contextInfo: { externalAdReply: { showAdAttribution: true,
 mediaType:  2,
 mediaUrl: `${url}`,
 title: `AUDIO IS BEING SENT...`,
 body: wm,
-sourceUrl: 'http://wa.me/254114148625', thumbnail: await ( await conn.getFile(thumbnail)).data
+sourceUrl: 'http://wa.me/917605902011', thumbnail: await ( await conn.getFile(thumbnail)).data
   }
  } 
 })
   
   //let buttons = [{ buttonText: { displayText: '📽VIDEO' }, buttonId: `${usedPrefix}ytv ${url} 360` }]
-// let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
+ //let msg = await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: captvid, footer: author, buttons }, { quoted: m })
 
   const yt = await youtubedlv2(url).catch(async _ => await youtubedl(url))
-//const link = await yt.mp3['128kbps'].download()
+const link = await yt.audio['128kbps'].download()
   let doc = { 
-  mp3: 
+  audio: 
   { 
     url: link 
 }, 
-mimetype: 'mp3/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
+mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
 mediaType:  2,
 mediaUrl: url,
 title: title,
@@ -57,7 +58,7 @@ thumbnail: await(await conn.getFile(thumbnail)).data
                        }
   }
 
-  //return conn.sendMessage(m.chat, doc, { quoted: m })
+         // return conn.sendMessage(m.chat, doc, { quoted: m })
 	// return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
 return await conn.sendFile(m.chat, link, title + '.mp3', '', m, false, { asDocument: true })
 }
