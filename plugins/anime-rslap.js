8f0aa7dd926c8231f137e7f66235037cad1575ae
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-18 05:15:48 +0300

    KANAMBO-BOT

--- plugins/anime-rslap.js
+++ plugins/anime-rslap.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['slap @tag']
 handler.tags = ['rnime']
 handler.command = /^(slap|bofetada)$/i
-handler.diamond = true
+handler.diamond = false
 handler.group = true
 
 export default handler
