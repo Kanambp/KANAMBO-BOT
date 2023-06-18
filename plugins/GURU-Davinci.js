97556997ea72184b458c195eb8a96d4465da039f
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-18 03:39:44 +0300

    Update GURU-Davinci.js

--- plugins/GURU-Davinci.js
+++ plugins/GURU-Davinci.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `*This command generates image from texts*\n\n*—◉ 𝙴xample usage*\n*◉ ${usedPrefix + command} Netaji subhas bose*\n*◉ ${usedPrefix + command} hatsune miku best*`
 try {
 m.reply('*Processing image *')
-let tiores = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=${lolkeysapi}&text=${text}`)
+let tiores = await conn.getFile(`https://api.lolhuman.xyz/api/dall-e?apikey=85243751c7971adf29b6bbb7${lolkeysapi}&text=${text}`)
 await conn.sendFile(m.chat, tiores.data, null, null, m)
 } catch {
 throw `*INTERNAL ERROR*`
