fed7e4d550e3da9d621efb3e392068a2036c75db
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-18 04:52:42 +0300

    Create cuddle.js

---
+++ plugins/cuddle.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch'
+import { sticker } from '../lib/sticker.js'
+//import db from '../lib/database.js'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+	
+	 let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    if (!who) throw `✳️Tag or mention someone\n\n📌 Example : ${usedPrefix + command} @tag`
+    
+    let user = global.db.data.users[who]
+    let name = conn.getName(who) 
+   let name2 = conn.getName(m.sender) 
+   m.react(rwait)
+
+  let rki = await fetch(`https://api.waifu.pics/sfw/cuddle`)
+    if (!rki.ok) throw await rki.text()
+   let jkis = await rki.json()
+   let { url } = jkis
+   let stiker = await sticker(null, url, `(${name2}) cuddle`, `${name}`)
+   conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+   m.react('🤗') 
+   
+}
+
+handler.help = ['cuddle @tag']
+handler.tags = ['rnime']
+handler.command = /^(cuddle|beso)$/i
+handler.diamond = true
+handler.group = true
+
+export default handler
