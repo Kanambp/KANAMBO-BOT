7242eabb77e7addfbd9255d3a903304c914793df
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-18 05:13:24 +0300

    KANAMBO-BOT

--- plugins/anime-rkiss.js
+++ plugins/anime-rkiss.js
@@ -27,7 +27,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 handler.help = ['kiss @tag']
 handler.tags = ['rnime']
 handler.command = /^(kiss|beso)$/i
-handler.diamond = true
+handler.diamond = false
 handler.group = true
 
 export default handler
