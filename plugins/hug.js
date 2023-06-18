24a1462357d66110166fda7e3609ee2d7a8cebd8
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-18 04:49:43 +0300

    Update hug.js

--- plugins/hug.js
+++ plugins/hug.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
    let { url } = jkis
    let stiker = await sticker(null, url, `(${name2}) huged`, `${name}`)
    conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
-   m.react('😚') 
+   m.react('🫂') 
    
 }
 
