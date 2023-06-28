07fa917df9cd84258e6609685db64f61af1c6c42
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-06-28 19:07:52 +0300

    KANAMBO-BOT

--- plugins/tools-lyrics.js
+++ plugins/tools-lyrics.js
@@ -1,4 +1,4 @@
-import { lyrics, lyricsv2 } from '@lyrics-fetcher'
+import { lyrics, lyricsv2 } from '@bochilteam/scraper'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
