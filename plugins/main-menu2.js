d8b0c211a70098f00bd4460ca14c82e5681ee3d8
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-08-07 15:14:04 +0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 let handler = async function (m, { conn, text, usedPrefix }) {
   
 let m2 = `
-≡ _Use the commands with the prefix . 
+≡ _Use the commands with the prefix_ \`\`\`${usedPrefix}\`\`\` 
 
 ▢ ‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‌‎
 Ⓟ = Premium
@@ -17,7 +17,7 @@ let m2 = `
 ▢ .info
 ▢ .owner
 ▢ .donate
-▢ .gpkanambo
+▢ .gpguru
 ▢ .listprem
 ▢ .help
 ▢ .audios
@@ -219,18 +219,17 @@ let m2 = `
 ▢ $
 └───────────  
 
-Bot has more than 300 commands and as you know Kanambo i am lazy to write all
+Bot has more than 300 commands and i am lazy to write all
 `
-//const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
-    let pp = './kanambo.jpg' 
-    conn.sendButton(m.chat, m2, '▢ KANAMBO  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/donbel_ovibel\n', pp, [
+    /*let pp = './kanambo.jpg'
+    conn.sendButton(m.chat, m2, '▢ ᴳkanambo  ┃ ᴮᴼᵀ\n▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n', pp, [
       ['⏍ Info', `${usedPrefix}botinfo`],
-      ['⌬ Groups', `${usedPrefix}gpkanambo`]
-    ],m, rpyt)
-   
+      ['⌬ Groups', `${usedPrefix}gpguru`]
+    ],m, rpyt) */
+    conn.sendMessage(m.chat,{text: m2}, m.sender)
 }
 
-handler.help = ['m2']
+handler.help = ['audios']
 handler.tags = ['main']
 handler.command = ['menu2', 'help2', 'h2'] 
 
