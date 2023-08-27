e46b70b011f437c12453b44bdfacd85eee5c1736
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-08-27 08:17:03 +0300

    KANAMBO-BOT

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -57,9 +57,9 @@ thumbnail: await(await conn.getFile(thumbnail)).data
                        }
   }
 
-   return conn.sendMessage(m.chat, doc, { quoted: m })
- // return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
-   // return await conn.sendFile(m.chat, link, title + '.mp3', '', m, true, { asDocument: true })
+  // return conn.sendMessage(m.chat, doc, { quoted: m })
+ return conn.sendMessage(m.chat, { document: { url: link }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, { quoted: m})
+  // return await conn.sendFile(m.chat, link, title + '.mp3', '', m, true, { asDocument: true })
 }
 handler.help = ['play'].map(v => v + ' <query>')
 handler.tags = ['downloader']
