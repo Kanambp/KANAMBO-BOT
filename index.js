d0db40fff5259e975fd081574a4e63b90e5d0c38
Peter Oyugi Odongo <106575586+Kanambp@users.noreply.github.com>
2023-11-06 00:57:13 +0300

    KANAMBO BOT

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('ABHISHEK-SER', {
+say('KANAMBO-BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
 })
-say(`'${name}' By @ABHISHEK SURESH._`, {
+say(`'${name}' By @PETER KANAMBO._`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
